0e2c4c59213d35951667246e9be0b27259f755e4
Johnson Kaho Poon <kahopoon@gmail.com>
2016-03-22 14:47:58 +0800

    first commit

--- fileTest/ViewController.swift
+++ fileTest/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var dataShow: UITextView!
+    var data:[String] = []
+    let destination = ((NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)).first! as NSString).stringByAppendingPathComponent("/data.txt")
+    
+    func updataUpload() {
+        userInput.text?.removeAll()
+        view.endEditing(true)
+        dataShow.text = data.joinWithSeparator("\n\n")
+        NSArray(array: data).writeToFile(destination, atomically: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let readData = NSArray(contentsOfFile: destination) {
+            for row in readData {
+                data.append(row as! String)
+            }
+            updataUpload()
+        }
     }
 
+    @IBAction func fileAccess(sender: AnyObject) {
+        data.append(userInput.text!)
+        updataUpload()
+    }
+    
+    @IBAction func dataDelete(sender: AnyObject) {
+        data.removeAll()
+        updataUpload()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
